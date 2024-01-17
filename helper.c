771deb3cc587ecc4b101fa55c0388cb79e512fe0
Xcutor <Lekanabass50@gmail.com>
2024-01-17 17:41:40 -0600

    task done

--- helper.c
+++ helper.c
@@ -57,6 +57,7 @@ int runCommand(info_t *info)
 	instruction_t commands[] = {
 		{"push", &push},
 		{"pall", &pall},
+		{"pint", &pint},
 		{NULL}
 	};
 
@@ -70,6 +71,30 @@ int runCommand(info_t *info)
 	}
 	fprintf(stderr, "L%d: unknown instruction %s", info->line, info->command[0]);
 	return (-1);
+}
+
+/**
+ * pint - prints the top of the stack
+ *
+ * @line_number: the current line
+ * @stack: the program stack
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	current  = *stack;
+	while (current->next != NULL)
+		current = current->next;
+
+	printf("%d\n", current->n);
 }
 
 /**
