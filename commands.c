2c3e132baa39c176d1ca61f07440f8c27d9dbbe2
Xcutor <Lekanabass50@gmail.com>
2024-01-17 17:47:36 -0600

    task done

---
+++ commands.c
@@ -0,0 +1,23 @@
+/**
+ * pint - prints the top of the stack
+ *
+ * @line_number: the current line
+ * @stack: the program stack
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	current  = *stack;
+	while (current->next != NULL)
+		current = current->next;
+
+	printf("%d\n", current->n);
+}
--- helper.c
+++ helper.c
@@ -74,30 +74,6 @@ int runCommand(info_t *info)
 }
 
 /**
- * pint - prints the top of the stack
- *
- * @line_number: the current line
- * @stack: the program stack
- */
-
-void pint(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current;
-
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	current  = *stack;
-	while (current->next != NULL)
-		current = current->next;
-
-	printf("%d\n", current->n);
-}
-
-/**
  * push - pushes an element onto the stack
  *
  * @line_number: the current line
