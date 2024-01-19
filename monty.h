b0a0cc8981044aa23f56ccec5e4e41a1f3782444
Xcutor <Lekanabass50@gmail.com>
2024-01-19 00:13:27 -0600

    task done

--- monty.h
+++ monty.h
@@ -81,6 +81,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 int runCommand(info_t *info);
 int readCode(info_t *info);
 int getLine(char **lineptr, size_t *buffersize, FILE *stream);
---
+++ moreCommands.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * adds adds the top two elements of the stack
+ *
+ * @stack: the current stack
+ * @line_number: current line number
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+	int result, i;
+
+	current = *stack;
+	for (i = 1; *stack != NULL && current->next != NULL;
+			current = current->next, i++);
+
+	if (i < 2)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n",
+				line_number);
+		freeStack();
+		freeMem();
+		exit(EXIT_FAILURE);
+	}
+
+	result = current->n + current->prev->n;
+	current->prev->n = result;
+	current->prev->next = NULL;
+	free(current);
+}
